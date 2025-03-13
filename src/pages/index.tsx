1eb9eed4d0c72998a9d667218d07e8510898063f
Victor Moya Cueto <victor.moya@mimacom.com>
2025-03-13 09:26:54 +0100

    Add static image of band to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
 import { type HeadFC, type PageProps } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import { Video } from '../components';
 import { Header, TourSection, ContactSection, Footer } from '../sections';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <div className="container flex mx-auto px-4 flex-col min-h-screen">
+    <div className="container flex mx-auto px-4 flex-col min-h-screen overflow-hidden">
       <Header />
       <main className="flex-grow text-center mt-10">
         <Video videoId="aysl7h5wQow" title="Showdown" />
         <TourSection />
+        <StaticImage
+          src="../images/band.png"
+          alt="Polaroid-style photo of the three members of the band in a music studio, with instruments and equipment in the background."
+        />
         <ContactSection />
       </main>
       <Footer />
